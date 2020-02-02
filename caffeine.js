d84411cc4294961e917b02f78d7c1b951fd72a61
Ryan <ryanjchen2@gmai.com>
2020-02-02 12:30:40 -0500

    Latest changes to caffeine graph

--- caffeine.js
+++ caffeine.js
@@ -5,6 +5,7 @@ function init() {
         callback: showInfo,
         simpleSheet: true } )
 }
+
 function responsivefy(svg) {
     // get container + svg aspect ratio
     var container = d3.select(svg.node().parentNode),
@@ -31,151 +32,67 @@ function responsivefy(svg) {
         svg.attr("height", Math.round(targetWidth / aspect));
     }
 }
+
 function showInfo(data, tabletop) {
-    // Hide spinner once data loads
-    var x = document.getElementById("spinner");
-    x.style.display = "none";
+    var spinner_div = document.getElementById("spinner");
+    spinner_div.style.display = "none";
 
     // Use tabletop to scrape sheet data
     data = tabletop.sheets("2020")['elements'];
-
-    // Get length of data
-    var n = data.length;
 
     // Populate dataset
     var totalListTMP = [];
     var dataset = [];
     for(var row in data){
-        dataset.push({"date": new Date(data[row]["Date"]), "total": data[row]["Total"], "boba": data[row]["Boba"]});
+        dataset.push({"date": new Date(data[row]["Date"]), "total": data[row]["Total"], "opacity": data[row]["PERCENT_MAX"]});
         totalListTMP.push(data[row]["Total"]);
     }
 
-    console.log("BOBS");
-
-    var yMax = Math.max(...totalListTMP) + 1;
-    //console.log(yMax);
-
-    // Set margins!
-    // TODO: Test on mobile
     var margin = {top: 50, right: 50, bottom: 50, left: 50}
         , width = window.innerWidth/2
         , height = (window.innerHeight - margin.top - margin.bottom)/2;
 
-
-    if(window.innerWidth < 1200) width = window.innerWidth*0.75;
-
-    // Get x scale all set
-    var firstLastDate = [new Date(data[0].Date), new Date(data[data.length-1].Date)];
-    var xScale = d3.scaleTime()
-        .domain(firstLastDate)
-        .range([0, width]);
-
-    // Get y scale all set
-    var yScale = d3.scaleLinear()
-        .domain([0, 8]) // input
-        .range([height, 0]); // output
-
-    // Update last update text
-    console.log(firstLastDate[1].toDateString());
-    d3.select("#lastUpdate").text("Last updated " + firstLastDate[1].toDateString() + ".");
-
-    // Generate the line
-    var line = d3.line()
-        .x(function(d) { return xScale(d.date); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d.total); }) // set the y values for the line generator
-        .curve(d3.curveCatmullRom.alpha(0.5));
-
-    var lineBoba = d3.line()
-        .x(function(d) { return xScale(d.date); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d.boba); }) // set the y values for the line generator
-        .curve(d3.curveCatmullRom.alpha(0.5));
-
-    // Div for tooltip
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-    var svg = d3.select("#graph").append("svg")
+    var svgContainer = d3.select("#graph").append("svg")
         .attr("width", width+margin.left+margin.right)
         .attr("height", height+margin.top+margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-        .call(responsivefy);
+        .style("font-family", "'Open Sans', sans-serif;")
 
-    // title text
-    svg.append("text")
-        .attr("x", (width / 2))             
-        .attr("y", 0 - (margin.top / 2))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "16px") 
-        .style("text-decoration", "underline")  
-        .text("Total cups of caffeine drank");
+    square_width = Math.floor((width+margin.left+margin.right)/10)*10
+    square_height = Math.floor((height+margin.top+margin.bottom)/10)*10
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottoma
+    var x = 0;
+    var y = 0;    
+    dataset.forEach(data => {
+        svgContainer.append("rect")
+                    .attr("x", x)
+                    .attr("y", y)
+                    .attr("width", 100)
+                    .attr("height", 100)
+                    .style("opacity", data["opacity"])
+                    .style("stroke-width", 2)
+                    .style("stroke", "#000000")
+                    .style("fill", "#fcba03")
+        
+        var date_format = data.date.toDateString().split(" ")[1] + " " + data.date.toDateString().split(" ")[2]
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
-
-    // 9. Append the path, bind the data, and call the line generator
-    svg.append("path")
-        .datum(dataset) // 10. Binds data to the line
-        .attr("class", "line") // Assign a class for styling
-        .attr("d", line); // 11. Calls the line generator
-
-    // 9. Append the path, bind the data, and call the line generator
-    svg.append("path")
-        .datum(dataset) // 10. Binds data to the line
-        .attr("class", "lineBoba") // Assign a class for styling
-        .attr("d", lineBoba); // 11. Calls the line generator
-
-    // 12. Appends a circle for each datapoint
-    svg.selectAll(".dot")
-        .data(dataset)
-        .enter().append("circle") // Uses the enter().append() method
-        .attr("class", "dot") // Assign a class for styling
-        .attr("cx", function(d) { return xScale(d.date) })
-        .attr("cy", function(d) { return yScale(d.total) })
-        .attr("r", 5)
-        .on("mouseover", function(d) {
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div	.html(d.date.toDateString() + "<br/>Total:"  + d.total)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            div.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
-
-    svg.selectAll(".dot")
-        .data(dataset)
-        .enter().append("circle") // Uses the enter().append() method
-        .attr("class", "dotBoba") // Assign a class for styling
-        .attr("cx", function(d) { return xScale(d.date) })
-        .attr("cy", function(d) { return yScale(d.boba) })
-        .attr("r", 5)
-        .on("mouseover", function(d) {
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div .html(d.date.toDateString() + "<br/>Boba:"  + d.boba)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            div.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
-
-
+        svgContainer.append("text")
+                    .attr("x", x+5)
+                    .attr("y", y+15)
+                    .text(function () { return date_format; })
+                    .attr("font-size", "13px")
+                    .style("font-weight", "bold")
+        
+        svgContainer.append("text")
+                    .attr("x", x+5)
+                    .attr("y", y+30)
+                    .text(function () { return data.total + " cups"; })
+                    .attr("font-size", "13px")
+        x += 100
+        if(x > square_width-100){
+            x = 0
+            y += 100
+        }
+    })
 
 }
 
